521cc3f3b94f5a4d6bbb90119adda7dd1eeec684
naveen-m0804 <mnaveenm2004@gmail.com>
2024-05-09 09:36:07 +0530

    first commit

--- Assets/detectCollision.cs
+++ Assets/detectCollision.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveForward : MonoBehaviour
+public class detectCollision : MonoBehaviour
 {
-    public float speed = 40.0f;
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        
     }
-
 }
